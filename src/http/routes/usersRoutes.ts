ff30153bfce01ebc80a108a83d103c604c4fe095
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-10-11 09:35:15 -0300

    Create usersRoutes.ts

---
+++ src/http/routes/usersRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { verifyJwt } from '../middleware/verify-jwt'
+import { UserController } from '../controllers'
+
+const userRouter = Router()
+
+// userRouter.post('/users', verifyJwt, UserController.createUser)
+// userRouter.get('/users', verifyJwt, UserController.getAllUsers)
+// userRouter.get('/users/:id', verifyJwt, UserController.getByIdUser)
+// userRouter.patch('/users/:id', verifyJwt, UserController.updateUser)
+// userRouter.delete('/users/:id', verifyJwt, UserController.deleteUser)
+
+export { userRouter }
