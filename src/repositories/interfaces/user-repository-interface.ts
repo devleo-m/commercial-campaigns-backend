93db244452c4a5553e028179cd382692889f1542
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-10-13 19:27:12 -0300

    factoring and improving - following project standard

--- src/repositories/interfaces/user-repository-interface.ts
+++ src/repositories/interfaces/user-repository-interface.ts
@@ -1,9 +1,10 @@
-import { IUser } from 'commercial-campaigns-db/src/interface'
+import { IUser } from 'commercial-campaigns-db/out/interface'
 
 export interface IUserRepository {
     create(userData: Partial<IUser>): Promise<IUser>
     getAll(where: object, orderBy: [string, string][]): Promise<IUser[]>
     getById(where: object): Promise<IUser | null>
+    getByEmail(email: string): Promise<IUser | null>
     update(id: number, userData: Partial<IUser>): Promise<IUser | null>
     delete(id: number): Promise<boolean>
 }
