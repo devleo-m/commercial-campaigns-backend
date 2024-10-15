969aef7b1313d2d528decb507a4b45b5dca17b20
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-10-15 09:52:07 -0300

    Update user-repository-database.ts

--- src/repositories/postgres/user-repository-database.ts
+++ src/repositories/postgres/user-repository-database.ts
@@ -1,45 +1,36 @@
-import { IUserRepository } from '../interfaces'
+import { CreateUserDto, IUserRepository, UpdateUserDto } from '../interfaces'
 import { IUser } from 'commercial-campaigns-db/src/interface'
 import { User } from 'commercial-campaigns-db/src/models'
 
 export class UserRepositoryDatabase implements IUserRepository {
-    async getById(where: object): Promise<IUser | null> {
-        return await User.findOne({
-            where: { ...where }
-        })
+    async getById(id: number): Promise<IUser | null> {
+        return await User.findOne({ where: { id } })
     }
 
-    async getAll(where: object, orderBy: [string, string][]): Promise<IUser[]> {
-        return await User.findAll({
-            raw: true,
-            where: { ...where },
-            order: orderBy
-        })
+    async getAll(where: object, orderBy: any[]): Promise<IUser[]> {
+        return await User.findAll({ raw: true, where: { ...where }, order: orderBy })
     }
 
     async getByEmail(email: string): Promise<IUser | null> {
-        return await User.findOne({
-            where: { email }
-        })
+        return await User.findOne({ where: { email } })
     }
 
-    async create(userData: Partial<IUser>): Promise<IUser> {
-        return await User.create(userData)
+    async create(user: CreateUserDto): Promise<IUser> {
+        return await User.create({ ...user })
     }
 
-    async update(id: number, userData: Partial<IUser>): Promise<IUser | null> {
-        const user = await this.getById({ id })
-        if (!user) return null
+    async update(id: number, user: UpdateUserDto): Promise<IUser> {
+        const [, [updateUser]] = await User.update(user,
+            {
+                where: { id },
+                returning: true
+            }
+        )
 
-        await user.update(userData)
-        return user
+        return updateUser
     }
 
     async delete(id: number): Promise<boolean> {
-        const user = await this.getById({ id })
-        if (!user) return false
-
-        await user.destroy()
-        return true
+        return !!await User.destroy({ where: { id } })
     }
 }
