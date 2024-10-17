45fde89f1f06249aa8300ca1a6038bc0ea700d81
devleo-m <149954966+devleo-m@users.noreply.github.com>
2024-10-17 14:39:06 -0300

    Update index.ts

--- src/repositories/postgres/index.ts
+++ src/repositories/postgres/index.ts
@@ -1,3 +1,4 @@
 export * from './user-repository-database'
 export * from './campaigns-repository-database'
 export * from './advertiserCommercials-repository-database'
+export * from './advertiserCommercialCampaignAssociations-repository-database'
