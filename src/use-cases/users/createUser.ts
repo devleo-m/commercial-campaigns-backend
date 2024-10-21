b82c06dd369dc654b4ac0c594902ae1cba1294f8
devleo-m <149954966+devleo-m@users.noreply.github.com>
2024-10-21 17:14:36 -0300

    Update createUser.ts

--- src/use-cases/users/createUser.ts
+++ src/use-cases/users/createUser.ts
@@ -1,6 +1,8 @@
 import { IUserRepository } from '../../repositories/interfaces'
 import { NotFoundError } from '../../utils/error/errors'
 import bcrypt from 'bcrypt'
+import { Transaction } from 'sequelize'
+import { sequelize as database  } from 'commercial-campaigns-db/out/database'
 
 type Input = {
     name: string,
@@ -14,40 +16,51 @@ type Output = {
     email: string
 }
 
+export const createSequelizeTransaction = async () => {
+    return await database.transaction()
+}
+
 export class CreateUserUseCase {
     constructor(
         readonly userRepository: IUserRepository
     ) {}
 
     async execute(input: Input): Promise<Output> {
-        const { name, email, password } = input
+        const transaction: Transaction = await createSequelizeTransaction()
+        try {
+            const { name, email, password } = input
 
-        const emailExists = await this.userRepository.getByEmail(email)
+            const emailExists = await this.userRepository.getByEmail(email)
 
-        if (emailExists) {
-            throw new NotFoundError('Email already exists!')
-        }
+            if (emailExists) {
+                throw new NotFoundError('Email already exists!')
+            }
 
-        if (password.length < 6) {
-            throw new NotFoundError('Password must be at least 6 characters long!')
-        }
+            if (password.length < 6) {
+                throw new NotFoundError('Password must be at least 6 characters long!')
+            }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+            const hashedPassword = await bcrypt.hash(password, 10)
 
-        if (name.length < 3) {
-            throw new NotFoundError('Name must be at least 3 characters long!')
-        }
+            if (name.length < 3) {
+                throw new NotFoundError('Name must be at least 3 characters long!')
+            }
 
-        const newUser = await this.userRepository.create({
-            name,
-            email,
-            password: hashedPassword
-        })
+            const newUser = await this.userRepository.create({
+                name,
+                email,
+                password: hashedPassword
+            }, transaction)
 
-        return {
-            id: newUser.id,
-            name: newUser.name,
-            email: newUser.email
+            await transaction.commit()
+            return {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email
+            }
+        } catch (error) {
+            await transaction.rollback()
+            throw error
         }
     }
 }
